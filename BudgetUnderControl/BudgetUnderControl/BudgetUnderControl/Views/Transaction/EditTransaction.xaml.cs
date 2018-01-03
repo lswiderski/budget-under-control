de2b6c9ea09eee54421e38ef89297a1f5b339133
Lukasz Swiderski <lukasz@swiderski.xyz>
2018-01-03 22:49:36 +0100

    delete focus flow from Edit Transaction

--- BudgetUnderControl/BudgetUnderControl/BudgetUnderControl/Views/Transaction/EditTransaction.xaml.cs
+++ BudgetUnderControl/BudgetUnderControl/BudgetUnderControl/Views/Transaction/EditTransaction.xaml.cs
@@ -24,11 +24,6 @@ namespace BudgetUnderControl.Views
             }
             this.transactionId = transactionId;
             InitializeComponent();
-
-            amount.Completed += (object sender, EventArgs e) => { name.Focus(); };
-            name.Completed += (object sender, EventArgs e) => { type.Focus(); };
-            type.Unfocused += (object sender, FocusEventArgs e) => { account.Focus(); };
-            account.Unfocused += (object sender, FocusEventArgs e) => { categories.Focus(); };
         }
 
         async void OnEditButtonClicked(object sender, EventArgs args)
@@ -41,7 +36,6 @@ namespace BudgetUnderControl.Views
         {
             base.OnAppearing();
             vm.GetTransaction(transactionId);
-            amount.Focus();
         }
 
         async void OnDeleteButtonClicked(object sender, EventArgs args)

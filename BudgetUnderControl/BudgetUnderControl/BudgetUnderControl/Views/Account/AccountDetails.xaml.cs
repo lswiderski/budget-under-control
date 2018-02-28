bf7c59917376ef083912635dad0144ac660ac289
Lukasz Swiderski <lukasz@swiderski.xyz>
2018-02-28 19:28:30 +0100

    floating add button on account details page

--- BudgetUnderControl/BudgetUnderControl/BudgetUnderControl/Views/Account/AccountDetails.xaml.cs
+++ BudgetUnderControl/BudgetUnderControl/BudgetUnderControl/Views/Account/AccountDetails.xaml.cs
@@ -15,6 +15,7 @@ namespace BudgetUnderControl.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountDetails : ContentPage
     {
+        private FloatingActionButtonView fab;
         IAccountDetailsViewModel vm;
         int accountId;
 
@@ -27,6 +28,31 @@ namespace BudgetUnderControl.Views
             this.accountId = accountId;
 
             InitializeComponent();
+
+            InitFAB();
+        }
+
+        private void InitFAB()
+        {
+            fab = new FloatingActionButtonView()
+            {
+                ImageName = "ic_add.png",
+                ColorNormal = Color.FromHex("ff3498db"),
+                ColorPressed = Color.Black,
+                ColorRipple = Color.FromHex("ff3498db"),
+                Clicked = OnAddButtonClicked
+            };
+
+            // Overlay the FAB in the bottom-right corner
+            AbsoluteLayout.SetLayoutFlags(fab, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(fab, new Rectangle(1f, 1f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            absoluteLayout.Children.Add(fab);
+        }
+
+        protected async void OnAddButtonClicked(object sender, EventArgs args)
+        {
+            var addAccount = new AddTransaction();
+            await Navigation.PushModalAsync(addAccount);
         }
 
         private async void Button_Edit_Clicked(object sender, EventArgs e)
